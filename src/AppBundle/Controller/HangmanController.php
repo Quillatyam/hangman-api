b248f06ac7692337dc8f7e87614d635762cee784
Jasper de Vries <Quillatyam@gmail.com>
2016-01-05 11:47:30 +0100

    Implement Guess Actions & Game Overview

--- src/AppBundle/Controller/HangmanController.php
+++ src/AppBundle/Controller/HangmanController.php
@@ -5,23 +5,24 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Hangman;
+use Symfony\Component\Debug\Debug;
 
 class HangmanController extends Controller
 {
-    
+
     /**
      * @Route("/games")
      */
     public function gamesAction() {
         $hangmen = $this->getDoctrine()->getRepository('AppBundle:Hangman')->findAll();
         $data = array();
-        foreach($hangmen as $hangman) {
-            $data[] = array('id' => $hangman->getId(), 'word' => $hangman->getWord(), 'tries_left' => $hangman->getTriesLeft(), 'status' => $hangman->getStatus(), 'guessed_letters' => $hangman->getGuessed());
+        foreach ($hangmen as $hangman) {
+            $data[] = $hangman->getArray($hangman->getTriesLeft() > 0);
         }
-        
+
         return new JsonResponse($data);
     }
-    
+
     /**
      * @Route("/game/{id}", defaults={"id" = "new"})
      */
@@ -29,26 +30,62 @@ class HangmanController extends Controller
         if ($id === 'new') {
             $hangman = new Hangman();
             $em = $this->getDoctrine()->getManager();
-            
+
             $em->persist($hangman);
             $em->flush();
-        } 
+        }
         else {
             $hangman = $this->getDoctrine()->getRepository('AppBundle:Hangman')->find($id);
         }
-        $data = array('id' => $hangman->getId(), 'word' => $hangman->getWord(), 'tries_left' => $hangman->getTriesLeft(), 'status' => $hangman->getStatus(), 'guessed_letters' => $hangman->getGuessed());
+        $data = $hangman->getArray($hangman->getTriesLeft() > 0);
         return new JsonResponse($data);
     }
-    
+
     /**
      * @Route("/game/{id}/guess/{letter}", requirements={
      *     "letter": "[a-z]{1}"
      *     })
      */
     public function guessAction($id, $letter) {
-        
-        $data = array('word' => 'aw.so..', 'tries_left' => 11, 'status' => 'busy', 'guessed_letters' => array('a', 'w', 's', 'o',),);
-        
+        $hangman = $this->getDoctrine()->getRepository('AppBundle:Hangman')->find($id);
+        if (!in_array($letter, $hangman->getGuessed())) {
+            $hangman->addGuessed($letter);
+            $word = str_split($hangman->getWord());
+            if (!in_array($letter, $word)) {
+                $hangman->setTriesLeft($hangman->getTriesLeft() - 1);
+            }
+        }
+        $hangman->updateStatus();
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($hangman);
+        $em->flush();
+        $data = $hangman->getArray($hangman->getTriesLeft() > 0);
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/game/{id}/guess/word/{word}")
+     */
+    public function guessWordAction($id, $word) {
+        $hangman = $this->getDoctrine()->getRepository('AppBundle:Hangman')->find($id);
+
+        if ($word === $hangman->getWord(false)) {
+            $hangman->setGuessed(str_split($word));
+            $hangman->setStatus('success');
+        }
+        else {
+            $hangman->setTriesLeft($hangman->getTriesLeft() - 1);
+        }
+        $hangman->updateStatus();
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($hangman);
+        $em->flush();
+        $data = $hangman->getArray($hangman->getTriesLeft() > 0);
+
         return new JsonResponse($data);
     }
 }
--- src/AppBundle/Entity/Hangman.php
+++ src/AppBundle/Entity/Hangman.php
@@ -5,47 +5,75 @@ use Symfony\Component\Finder\Finder;
 
 /**
  * Hangman
+ * @ORM\Entity()
+ * @ORM\HasLifecycleCallbacks()
  */
 class Hangman
 {
-    
+
     /**
      * @var integer
      */
     private $id;
-    
+
     /**
      * @var string
      */
     private $word;
-    
+
     /**
      * @var integer
      */
     private $tries_left;
-    
+
     /**
      * @var string
      */
     private $status;
-    
+
     /**
      * @var array
      */
     private $guessed;
-    
+
     public function __construct() {
         $finder = new Finder();
         $finder->in(__DIR__)->files()->name(chr(rand(65, 90)) . ' Words.txt');
         $finder = $finder->getIterator();
         $finder->rewind();
-        $words = file(dirname(__FILE__).'/'.$finder->current()->GetRelativePathname()); 
-        $this->word = str_replace(array("\r\n","\r","\n",), '', $words[array_rand($words)]);
+        $words = file(dirname(__FILE__) . '/' . $finder->current()->GetRelativePathname());
+        $this->word = str_replace(array("\r\n", "\r", "\n",), '', $words[array_rand($words) ]);
         $this->tries_left = 11;
         $this->status = 'busy';
         $this->guessed = array();
     }
-    
+
+    /**
+     * Get Hangman as array
+     *
+     * @return array
+     */
+    public function getArray($obfuscate = true) {
+        $data = array();
+        $data['id'] = $this->id;
+        $data['tries_left'] = $this->tries_left;
+        $data['status'] = $this->status;
+        $data['guessed_letters'] = $this->guessed;
+        $data['word'] = $this->getWord($obfuscate);
+        return $data;
+    }
+
+    public function updateStatus() {
+        if ($this->status == 'busy') {
+            if ($this->tries_left === 0) {
+                $this->status = 'fail';
+            }
+            elseif ($this->getWord() === $this->getWord(false)) {
+                $this->status = 'success';
+            }
+        }
+    }
+
     /**
      * Get id
      *
@@ -54,7 +82,7 @@ class Hangman
     public function getId() {
         return $this->id;
     }
-    
+
     /**
      * Set word
      *
@@ -64,20 +92,30 @@ class Hangman
      */
     public function setWord($word) {
         $this->word = $word;
-        
+
         return $this;
     }
-    
+
     /**
      * Get word
      *
      * @return string
      */
-    public function getWord() {
-        if (count($this->guessed) > 0) return preg_replace('/[^' . implode('', $this->guessed) . ']+/', '', $this->word);
-        else return $this->word;
+    public function getWord($obfuscate = true) {
+        $word = $this->word;
+        if ($obfuscate) {
+            $word = str_split($word);
+            $replace = array();
+            foreach ($word as $letter) {
+                if (!in_array($letter, $this->guessed)) {
+                    $replace[] = $letter;
+                }
+            }
+            $word = str_replace($replace, '.', $this->word);
+        }
+        return $word;
     }
-    
+
     /**
      * Set triesLeft
      *
@@ -87,10 +125,10 @@ class Hangman
      */
     public function setTriesLeft($triesLeft) {
         $this->tries_left = $triesLeft;
-        
+
         return $this;
     }
-    
+
     /**
      * Get triesLeft
      *
@@ -99,7 +137,7 @@ class Hangman
     public function getTriesLeft() {
         return $this->tries_left;
     }
-    
+
     /**
      * Set status
      *
@@ -109,10 +147,10 @@ class Hangman
      */
     public function setStatus($status) {
         $this->status = $status;
-        
+
         return $this;
     }
-    
+
     /**
      * Get status
      *
@@ -121,7 +159,7 @@ class Hangman
     public function getStatus() {
         return $this->status;
     }
-    
+
     /**
      * Set guessed
      *
@@ -131,10 +169,23 @@ class Hangman
      */
     public function setGuessed($guessed) {
         $this->guessed = $guessed;
-        
+
         return $this;
     }
-    
+
+    /**
+     * Set guessed
+     *
+     * @param string $letter
+     *
+     * @return Hangman
+     */
+    public function addGuessed($letter) {
+        $this->guessed[] = $letter;
+
+        return $this;
+    }
+
     /**
      * Get guessed
      *
